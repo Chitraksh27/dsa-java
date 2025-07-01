9f0ec93555246f9a8fa27b34a6d7ee3c52a2b80b
Chitraksh Sharma <chitraksh73278@gmail.com>
2025-07-01 12:53:02 +0530

    Added Question

---
+++ Arrays/LeftRotatebyD.java
@@ -0,0 +1,32 @@
+// Java program to left rotate an array by d positions
+
+public class LeftRotatebyD {
+    static void reverseArray(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    static void rotateArr(int[] arr, int d) {
+        int n = arr.length;
+
+        d = d % n;
+
+        reverseArray(arr, 0, d - 1);
+        reverseArray(arr, d, n - 1);
+        reverseArray(arr, 0, n - 1);
+    }
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int d = 2;
+
+        rotateArr(arr, d);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
