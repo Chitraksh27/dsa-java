62e238f7001d517e09a30cd04947316b53af8c87
Chitraksh Sharma <chitraksh73278@gmail.com>
2025-07-02 16:10:23 +0530

    Added Questions

---
+++ Arrays/StockBuyAndSell1.java
@@ -0,0 +1,19 @@
+// Java program to find the maximum profit from stock buy and sell if multiple transactions are allowed.
+public class StockBuyAndSell1 {
+    static int maximumProfit(int[] prices) {
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {100, 180, 260, 310, 40, 535, 695};
+
+        System.out.println(maximumProfit(prices));
+    }
+}
